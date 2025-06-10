05c99cbfa8120b0a5a646935806011f3601d4460
ranni-sys <ranni.voyage@gmail.com>
2025-06-10 10:14:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('interactionCreate', async interaction => {
     try {
       await interaction.deferReply({ ephemeral: true });
 
-      const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout after 3s')), 3000));
+      const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout after 3s')), 1000));
 
       const data = await Promise.race([
         handlePTInfo(ptNumber),
